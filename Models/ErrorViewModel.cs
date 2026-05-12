fcc1d98ce6e4d3fe4ce26b044b5a7b764f2fdaee
Fırat Can Karakoç <firatcankarakoc@gmail.com>
2026-05-12 19:16:09 +0300

    MVC şablonu kuruldu. Temiz başlangıç ve kodlama için hazır.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Burs_Takip_Sistemi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
