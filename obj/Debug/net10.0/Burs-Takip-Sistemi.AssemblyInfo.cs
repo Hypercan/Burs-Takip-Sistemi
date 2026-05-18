d731552dbf79dcfaf3b38f94124c3f36d51057cf
Fırat Can Karakoç <firatcankarakoc@gmail.com>
2026-05-18 17:25:40 +0300

    Değişiklik 1

--- obj/Debug/net10.0/Burs-Takip-Sistemi.AssemblyInfo.cs
+++ obj/Debug/net10.0/Burs-Takip-Sistemi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Burs-Takip-Sistemi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66c0660c924523d2f2f8af1bb66d47f76a6903b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b6126dd161c523c9bc5eb09b14689de4d5c3a44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Burs-Takip-Sistemi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Burs-Takip-Sistemi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
